01c53f8e36876c7cdf13dadef4fbfa98827476a7
agatajuengst <agata.juengst@gmail.com>
2026-01-19 13:07:52 +0100

    Update heygen-token.js

--- api/heygen-token.js
+++ api/heygen-token.js
@@ -12,8 +12,8 @@ export default async function handler(req, res) {
 
     // per URL steuerbar:
     // .../api/heygen-token?avatar_id=UUID&context_id=UUID
-    const avatarId = url.searchParams.get("avatar_id") || "HIER_DEFAULT_AVATAR_UUID";
-    const contextId = url.searchParams.get("context_id") || "HIER_DEFAULT_CONTEXT_UUID";
+    const avatarId = url.searchParams.get("avatar_id") || "bb1f6ebc-b388-4a39-9e2b-8df618e0377c";
+    const contextId = url.searchParams.get("context_id") || "0316a6c3-2197-49dc-9c78-1aa7b290fec8";
 
     const body = {
       mode: "FULL",
